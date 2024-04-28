1423600bbe30729fde7daa9d159f9092958aedb8
unknown <jelikapalad@gmail.com>
2024-04-28 22:43:55 +0800

    new

---
+++ app/Views/user/about.php
@@ -0,0 +1,85 @@
+<main class="w-full">
+    <section role="history" class="flex flex-col md:flex-row bg-zinc-100 text-black py-2">
+        <div class=" flex justify-center items-center w-full md:w-2/5">
+            <img class="w-3/5 md:w-1/2" src="<?= base_url('./images/logo.png') ?>" alt="image" srcset="">
+        </div>
+        <div class="flex flex-col justify-around items-center md:items-start gap-4 md:gap-0 w-full md:w-1/2 px-8">
+            <div class=" flex flex-col gap-4 items-center md:items-start py-3 md:p-0">
+                <span class=" font-semibold text-xl">History</span>
+                <p class="text-justify">
+                The Parish of Saint John of the Cross, established on March 18, 1992, encompasses Barangays Pembo and Rizal in Makati. 
+                On July 30, 1999 a parish resolution affecting the start of the construction of a new Parish Church was passed and 
+                approved by the members of the Parish Pastoral Council, and the cornerstone was laid on August 2, 1999, with the 
+                presence of His Eminence Jaime Cardinal Sin, Archbishop of Manila. The parish, originating in the 1950’s with members 
+                of the Scout Rangers Regiment, evolved from Panther's Enlisted Men's Barrio (PEMBO) into the largest barrio in Fort 
+                Bonifacio by the 1960s. Initially part of Mater Dolorosa Parish in 1987 brought significant changes to the community. <a href="#">See more</a>.
+                </p>
+            </div>
+        </div>
+    </section>
+    <section role="priest&staff" class=" grid grid-rows-2 gap-2 bg-slate-950 text-zinc-100 py-2">
+        <div class="flex flex-col md:flex-row">
+            <div class=" flex justify-center items-center">
+                <img class=" block w-6/12" src="<?= base_url('./images/logo.png') ?>" alt="image" srcset="">
+            </div>
+            <div class=" flex flex-col justify-around items-center md:items-start gap-4 md:gap-0 w-full md:w-1/2 px-8">
+                <div class=" flex flex-col gap-4 items-center md:items-start py-3 md:p-0">
+                    <span class=" font-semibold text-xl">About the priest</span>
+                    <p class=" text-justify">
+                    Rev. Fr. Clarito "Charlie" M. Jundis started preaching at St. John of the Cross Parish on November 23, 2022. Before preaching at there, 
+                    he was preaching in America where started his journey of priesthood back in 1978. He has been a priest for 46 years and is a builder, 
+                    responsible for constructing the beautiful church of Sto. Niño de Taguig Parish. He is the first priest to initiate the construction 
+                    of the church and is the founder of Sto. Niño de Taguig Catholic Church.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col items-center py-6">
+            <div>
+                <h1 class="card-title align-middle">Meet our staff</h1>
+            </div>
+            <div class="grid grid-cols-1 md:grid-cols-4 gap-2">
+                <div class="card w-96 bg-slate-950">
+                    <figure class="px-10 pt-10">
+                        <img class=" block w-6/12" src="<?= base_url('./images/logo.png') ?>" alt="image" srcset="">
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <h2 class="card-title align-middle">Staff 1</h2>
+                    </div>
+                </div>
+                <div class="card w-96 bg-slate-950">
+                    <figure class="px-10 pt-10">
+                        <img class=" block w-6/12" src="<?= base_url('./images/logo.png') ?>" alt="image" srcset="">
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <h2 class="card-title align-middle">Staff 1</h2>
+                    </div>
+                </div>
+                <div class="card w-96 bg-slate-950">
+                    <figure class="px-10 pt-10">
+                        <img class=" block w-6/12" src="<?= base_url('./images/logo.png') ?>" alt="image" srcset="">
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <h2 class="card-title align-middle">Staff 1</h2>
+                    </div>
+                </div>
+                <div class="card w-96 bg-slate-950">
+                    <figure class="px-10 pt-10">
+                        <img class=" block w-6/12" src="<?= base_url('./images/logo.png') ?>" alt="image" srcset="">
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <h2 class="card-title align-middle">Staff 1</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section role="map" class="flex bg-zinc-100 text-black py-2">
+        <div class="flex flex-col p-8">
+            <h2> Map (Google Maps)</h2>
+            <div class="w-full border-2 border-black">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.952147477856!2d121.05812207465668!3d14.544729178442969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c893b4f02ca1%3A0x571d8fe4a87168cf!2s9%20Sampaguita%20St%2C%20Taguig%2C%201218%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1700128810775!5m2!1sen!2sph" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </section>
+</main>
