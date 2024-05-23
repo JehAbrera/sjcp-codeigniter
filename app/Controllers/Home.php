eb0f434d579d85ddf09d1ec489fa8c1183f3df0b
LAPTOP-SD67TGVB\Jelika Palad <jelikapalad@gmail.com>
2024-05-24 03:07:44 +0800

    commit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,8 @@ use CodeIgniter\Exceptions\PageNotFoundException;
 class Home extends BaseController
 {
     protected $records;
-    public function __construct(){
+    public function __construct()
+    {
         $this->records = new \App\Models\Records();
     }
 
@@ -33,24 +34,23 @@ class Home extends BaseController
             // Whoops, we don't have a page for that!
             throw new PageNotFoundException($page);
         }
-<<<<<<< HEAD
 
         if ($page == 'faqs') {
             $addInf = [
                 'faqs' => $this->records->getAnnouncements($page)->paginate(10)
             ];
-=======
-        if ($page == 'success') {
-            return view('user/' . $page);
->>>>>>> 8bcc341068ff870b09d6c64cddf8da86611c4b4c
-        }
-        $data['title'] = ucfirst($page);
+            if ($page == 'success') {
+                return view('user/' . $page);
+            }
+            $data['title'] = ucfirst($page);
 
-        $data = array_merge($data, $addInf);
-        return view('templates/navbar', $data) . view('templates/header', $data) . view('user/' . $page) . view('templates/footer');
+            $data = array_merge($data, $addInf);
+            return view('templates/navbar', $data) . view('templates/header', $data) . view('user/' . $page) . view('templates/footer');
+        }
     }
 
-    public function terms(){
+    public function terms()
+    {
         $data['title'] = "TermsandServices";
         return view('/user/termsandservices', $data);
     }
